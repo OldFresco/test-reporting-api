cb7b6a86a72459d0c39693313207a2595c72471f
Kojo Hinson <hinsonkojo@gmail.com>
2016-10-20 17:09:20 +0100

    Move routes to controllers

---
+++ controllers/test.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+    router = express.Router()
+
+var TestResult = require('../app/models/result')
+
+router.route('/results')
+
+// create a result resouce (accessed at POST http://localhost:8080/test/results)
+.post(function (req, res) {
+    var result = new TestResult()
+
+    result.name = req.body.name;
+    result.version = req.body.version
+    result.environment = req.body.environment
+    result.testRunStartedAtTime = req.body.testRunStartedAtTime
+    result.testRunCompletedAtTime = req.body.testRunCompletedAtTime
+    result.overallResult = req.body.overallResult
+    result.triggeredBy = req.body.triggeredBy
+    result.triggeredFrom = req.body.triggeredFrom
+    result.xmlResults = req.body.xmlResults
+    result.htmlReport = req.body.htmlReport
+
+    result.save(function (err) {
+        if (err)
+            res.send(err)
+
+        res.json({
+            'createdAt': Date.now(),
+            'state': 'created',
+            'content': result,
+            'links': [{
+                'href': req.protocol + req.hostname + req.originalUrl,
+                'ref': 'self',
+                'method': 'POST'
+            }]
+        })
+    })
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -10,6 +10,9 @@ var mongoose = require('mongoose')
 var config = require('./config/config')
 var TestResult = require('./app/models/result')
 
+// set our port
+var port = process.env.PORT || 8080 
+
 // configure app
 app.use(morgan('dev')) // log requests to the console
 
@@ -17,58 +20,11 @@ app.use(morgan('dev')) // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-var port = process.env.PORT || 8080 // set our port
-mongoose.connect(config().MONGO_URI) // connect to our database
+// Register routes
+app.use(require('./controllers'))
 
-// ROUTES FOR OUR API
-// =============================================================================
-
-// create our router
-var router = express.Router()
-
-// Health check
-router.get('/health', function(req, res) {
-    res.json({ message: 'Ok' })
-})
-
-// on routes that end in /test/results
-// ----------------------------------------------------
-router.route('/results')
-
-// create a result resouce (accessed at POST http://localhost:8080/test/results)
-.post(function(req, res) {
-    var result = new TestResult()
-
-    result.name = req.body.name;
-    result.version = req.body.version
-    result.environment = req.body.environment
-    result.testRunStartedAtTime = req.body.testRunStartedAtTime
-    result.testRunCompletedAtTime = req.body.testRunCompletedAtTime
-    result.overallResult = req.body.overallResult
-    result.triggeredBy = req.body.triggeredBy
-    result.triggeredFrom = req.body.triggeredFrom
-    result.xmlResults = req.body.xmlResults
-    result.htmlReport = req.body.htmlReport
-
-    result.save(function(err) {
-        if (err)
-            res.send(err)
-
-        res.json({
-            'createdAt': Date.now(),
-            'state': 'created',
-            'content': result,
-            'links': [{
-                'href': req.protocol + req.hostname + req.originalUrl,
-                'ref': 'self',
-                'method': 'POST'
-            }]
-        })
-    })
-})
-
-// REGISTER OUR ROUTES -------------------------------
-app.use('/test', router)
+// connect to our database
+mongoose.connect(config().MONGO_URI)
 
 // START THE SERVER
 // =============================================================================
