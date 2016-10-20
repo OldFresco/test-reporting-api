dcb9093006fbf7a5d41995cd65e9b2db1ae838cf
Kojo Hinson <hinsonkojo@gmail.com>
2016-10-20 17:16:26 +0100

    add database name to config

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 module.exports = function() {
     switch (process.env.NODE_ENV) {
         case 'development':
-            return { MONGO_URI: 'mongodb://localhost' }
+            return { MONGO_URI: 'mongodb://localhost/testing' }
 
         case 'production':
-            return { MONGO_URI: 'mongodb://localhost' }
+            return { MONGO_URI: 'mongodb://localhost/testing' }
 
         default:
-            return { MONGO_URI: 'mongodb://localhost' }
+            return { MONGO_URI: 'mongodb://localhost/testing' }
     }
 }
